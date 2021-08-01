fd4bd3953cfaed4e93e8f68f7a299b3ce213928b
KrishnaKakarla88 <mk.kakarla@gmail.com>
2021-08-01 17:22:12 +0800

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## [HandleBars-Express-Session-Sequelize-mySQL-TechBlog_MVC] Changelog
+
+<a name="x.y.z"></a>
+# x.y.z (yyyy-mm-dd)
+
+*Features*
+* ...
+
+*Bug Fixes*
+* ...
+
+*Breaking Changes*
+* ...
---
+++ config/config.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: DB_PORT
+    });
+
+module.exports = sequelize;
+
+
+
